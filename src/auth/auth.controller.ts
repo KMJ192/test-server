51ad4dad1fbc2a21ca0a86e58c2569aaef012ae6
MyeongJunKim <myeongjun222@naver.com>
2023-05-06 15:44:17 +0900

    Update auth.controller.ts

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -9,22 +9,28 @@ export class AuthController {
   @Post('login')
   async login(
     @Body('email') email: string,
+    @Body('password') password: string,
     @Res({ passthrough: true }) response: Response,
   ) {
     const user = {
       email,
-      name: 'test',
+      password,
     };
 
-    // const jwt = await this.jwtService.signAsync({ id: 1 });
-    // response.cookie('jwt', jwt, { httpOnly: true });
-    return user;
+    const jwt = await this.jwtService.signAsync({ id: 1 });
+    response.cookie('jwt', jwt, { httpOnly: true });
+    return {
+      code: 'S',
+      message: '',
+      result: user,
+    };
   }
 
   @Post('logout')
   async logout(@Res({ passthrough: true }) response: Response) {
     response.clearCookie('jwt');
     return {
+      code: 'S',
       message: 'Success',
     };
   }
